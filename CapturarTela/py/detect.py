d582b14f915cc89305865a33b84b738ab28ac8f2
Daniel R. Gomes <dr987100@gmail.com>
2023-07-14 14:09:11 -0300

    Melhorias nas Boxes

--- CapturarTela/py/detect.py
+++ CapturarTela/py/detect.py
@@ -1,21 +1,42 @@
 from PIL import ImageGrab
 from ultralytics import YOLO
 
+import torch
 import numpy as np
 import cv2
 
+model = YOLO("yolov8n.pt")
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
 while (True):
-    img = ImageGrab.grab(bbox=(300,100, 800,800))
+
+    img = ImageGrab.grab(bbox=(0, 0, 1000,700))
 
     frame = np.array(img)
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    model = YOLO("yolov8n.pt")
+    
 
-    result = model.predict(source=img)
-    print(result)
-    #for detect in result:
+    result = model(img)
+    
+    for detect in result:
+
+        boxes = detect.boxes
+        for box in boxes:
+            x,y,w,h = box.xywh[0]
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        cv2.imshow("Frames", frame)
+
+    if (cv2.waitKey(0)):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
         
